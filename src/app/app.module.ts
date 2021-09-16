9d93ead4be3d28c248689c7267da57ab14d8b964
Alexander Gridin <alexander.m.gridin@gmail.com>
2021-09-16 18:46:23 +0300

    Refactor app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,45 +2,47 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GridModule } from '@progress/kendo-angular-grid';
-import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
-import { ButtonsModule } from '@progress/kendo-angular-buttons';
-import { InputsModule } from '@progress/kendo-angular-inputs';
-
-import { UsersStateService } from './services/users-state/users-state.service';
-import { UsersService } from './services/users/users.service';
-
-import { DialogsModule } from '@progress/kendo-angular-dialog';
 import { StoreModule } from '@ngrx/store';
-
 import { usersReducer } from './store/users/users.reducer';
+
+import { GridModule } from '@progress/kendo-angular-grid';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { DialogsModule } from '@progress/kendo-angular-dialog';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { UsersService } from './services/users/users.service';
+import { UsersStateService } from './services/users-state/users-state.service';
+
 import { UsersPageComponent } from './pages/users-page/users-page.component';
-import { PageTitleComponent } from './components/page-title/page-title.component';
+
+import { AppComponent } from './app.component';
+import { PageComponent } from './components/page/page.component';
 import { AddUserComponent } from './components/add-user/add-user.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { PageTitleComponent } from './components/page-title/page-title.component';
 import { ContainerComponent } from './components/container/container.component';
 import { UsersTableComponent } from './components/users-table/users-table.component';
-import { LabelModule } from '@progress/kendo-angular-label';
-import { PageComponent } from './components/page/page.component';
-import { UserFormComponent } from './components/user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersPageComponent,
-    PageTitleComponent,
+    PageComponent,
     AddUserComponent,
+    UserFormComponent,
+    PageTitleComponent,
     ContainerComponent,
     UsersTableComponent,
-    PageComponent,
-    UserFormComponent,
+    UsersPageComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
+
     DropDownsModule,
     GridModule,
     DateInputsModule,
@@ -48,6 +50,7 @@ import { UserFormComponent } from './components/user-form/user-form.component';
     ButtonsModule,
     InputsModule,
     DialogsModule,
+
     StoreModule.forRoot({ users: usersReducer }),
   ],
   providers: [UsersStateService, UsersService],
