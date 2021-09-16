3685336959aac0b4d12bb83f117b56cc0d89f252
Alexander Gridin <alexander.m.gridin@gmail.com>
2021-09-16 15:23:32 +0300

    Tiny refactoring

--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -16,8 +16,6 @@ export class UsersService {
       educationEndDate,
     } = addUserFormDataObject;
 
-    // educationEndDate = educationEndDate === null ? ;
-
     return {
       userName,
       gender,
