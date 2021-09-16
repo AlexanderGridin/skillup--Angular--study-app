0511fe0cc51e27d62178d4c2bc6e00f638c42c19
Alexander Gridin <alexander.m.gridin@gmail.com>
2021-09-16 17:19:56 +0300

    Refactor add-user component

--- src/app/components/add-user/add-user.component.ts
+++ src/app/components/add-user/add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { User } from 'src/app/interfaces/user';
 import { UsersStateService } from 'src/app/services/users-state/users-state.service';
 
@@ -7,15 +7,13 @@ import { UsersStateService } from 'src/app/services/users-state/users-state.serv
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.css'],
 })
-export class AddUserComponent implements OnInit {
+export class AddUserComponent {
   public isModalVisible: boolean = false;
   public modalTitle: string = 'Add user';
   public modalMinWidth: number = 250;
   public modalWidth: number = 450;
 
   constructor(private usersStateService: UsersStateService) {}
-
-  public ngOnInit(): void {}
 
   public handleFormSubmit(newUser: User): void {
     this.usersStateService.addUser(newUser);
